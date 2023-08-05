2ab309a4eb41045d60a14a799119d4275a20b596
Karina Karimova <karina.karimova@mooncascade.com>
2023-08-05 22:36:37 +0300

    Add contrast ratio test

--- OAT/Sources/OAT/UIViewSubclasses/UIButton+AccessibilityCheckable.swift
+++ OAT/Sources/OAT/UIViewSubclasses/UIButton+AccessibilityCheckable.swift
@@ -2,45 +2,43 @@ import UIKit
 
 extension UIButton: AccessibilityCheckable {
     func check() -> [any AccessibilityError] {
-        let button = self
         var errors: [AccessibilityError] = []
-        let defaultAccessibilityLabel = button.titleLabel?.text ?? button.currentTitle
-        let actualAccessibilityLabel = button.accessibilityLabel
+        let defaultAccessibilityLabel = self.titleLabel?.text ?? self.currentTitle
+        let actualAccessibilityLabel = self.accessibilityLabel
         
         if actualAccessibilityLabel == nil, defaultAccessibilityLabel == nil {
            errors.append(AccessibilityLabelError.labelIsMissing)
         } else if let actualAccessibilityLabel {
-            errors.append(contentsOf: button.checkAccessiblityLabel(actualAccessibilityLabel))
+            errors.append(contentsOf: self.checkAccessiblityLabel(actualAccessibilityLabel))
         } else if let defaultAccessibilityLabel {
-            errors.append(contentsOf: button.checkAccessiblityLabel(defaultAccessibilityLabel))
+            errors.append(contentsOf: self.checkAccessiblityLabel(defaultAccessibilityLabel))
         }
 
-        if let hint = button.accessibilityHint {
-            errors.append(contentsOf: button.checkAccessibilityHint(hint))
+        if let hint = self.accessibilityHint {
+            errors.append(contentsOf: self.checkAccessibilityHint(hint))
         }
         
-        if let hint = button.accessibilityHint, let label = button.accessibilityLabel ?? button.currentTitle {
+        if let hint = self.accessibilityHint, let label = self.accessibilityLabel ?? self.currentTitle {
             if hint.contains(label + " ") {
                 errors.append(AccessibilityHintError.containsLabel)
             }
         }
         
-        errors.append(contentsOf: checkTitleAndBackgroundColor(button))
+        errors.append(contentsOf: checkTitleAndBackgroundColor())
         
         return errors
     }
     
-    func checkTitleAndBackgroundColor(_ button: UIButton) -> [any AccessibilityError]  {
-        guard button.isEnabled,
-              let font = button.titleLabel?.font.pointSize,
-              let isBold = button.titleLabel?.font.fontDescriptor.symbolicTraits.contains(.traitBold)
+    func checkTitleAndBackgroundColor() -> [any AccessibilityError]  {
+        guard self.isEnabled,
+              let font = self.titleLabel?.font.pointSize,
+              let isBold = self.titleLabel?.font.fontDescriptor.symbolicTraits.contains(.traitBold)
         else { return [] }
         let conditionFont: CGFloat = isBold ? 14 : 18
         let minimumContrast = font < conditionFont ? 4.5 / 1 : 3.0 / 1
-        
-        if let backgroundColor = button.backgroundColor {
-            let actualContrast = UIColor.contrastRatio(between: button.currentTitleColor, and: backgroundColor)
-            print("actualContrast == \(actualContrast) \n minimumContrast == \(minimumContrast)")
+        let titleColor = self.titleLabel?.textColor ?? currentTitleColor
+        if let backgroundColor = self.backgroundColor {
+            let actualContrast = UIColor.contrastRatio(between: titleColor, and: backgroundColor)
             return actualContrast < minimumContrast ? [ContrastError.contrastRatioSmallText] : []
         } else {
             return []
