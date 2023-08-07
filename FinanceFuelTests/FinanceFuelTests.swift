a34d3b8216e4c1f402b47a89b5caeb3c43f3d7f6
Karina Karimova <karina.karimova@mooncascade.com>
2023-08-07 12:18:02 +0300

    Setup GTXilib Cocoa Pods

--- FinanceFuelTests/FinanceFuelTests.swift
+++ FinanceFuelTests/FinanceFuelTests.swift
@@ -1,7 +1,21 @@
 import XCTest
+import GTXiLib
+@testable import FinanceFuelSources
 
 final class FinanceFuelTests: XCTestCase {
-    func testExample() throws {
+    override class func setUp() {
+        super.setUp()
+//               let checksToBeInstalled: [GTXChecking] = GTXChecksCollection.allGTXChecks()
+        let checksToBeInstalled: [GTXChecking] = [GTXChecksCollection.checkForAXLabelNotPunctuated()]
+        let tmp = GTXTestSuite.init(allTestsIn: FinanceFuelTests.self)
+        GTXiLib.install(on: tmp ?? GTXTestSuite(), checks: checksToBeInstalled, elementExcludeLists: [])
+    }
 
+    func testExample() throws {
+        let button = UIButton()
+        button.setTitle("Ok.", for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 32, height: 32)
+        let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        keyWindow?.addSubview(button)
     }
 }
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '15.0'
+
+use_modular_headers!
+target 'FinanceFuelTests' do
+  pod "GTXiLib"
+end
+
