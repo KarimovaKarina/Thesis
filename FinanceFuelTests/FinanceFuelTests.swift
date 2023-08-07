8a92d3cf64d3b0aa8f15bb589c3a85b8f632129f
Karina Karimova <karina.karimova@mooncascade.com>
2023-08-07 14:20:29 +0300

    Failing GTXilib test example

--- FinanceFuelTests/FinanceFuelTests.swift
+++ FinanceFuelTests/FinanceFuelTests.swift
@@ -1,21 +1,34 @@
 import XCTest
 import GTXiLib
+import UIKit
 @testable import FinanceFuelSources
 
 final class FinanceFuelTests: XCTestCase {
+    let button = UIButton()
+
     override class func setUp() {
         super.setUp()
-//               let checksToBeInstalled: [GTXChecking] = GTXChecksCollection.allGTXChecks()
-        let checksToBeInstalled: [GTXChecking] = [GTXChecksCollection.checkForAXLabelNotRedundantWithTraits()]
+        let checksToBeInstalled: [GTXChecking] = GTXChecksCollection.allGTXChecks()
         let tmp = GTXTestSuite.init(allTestsIn: FinanceFuelTests.self)
         GTXiLib.install(on: tmp ?? GTXTestSuite(), checks: checksToBeInstalled, elementExcludeLists: [])
     }
 
     func testExample() throws {
-        let button = UIButton()
-        button.setTitle("button", for: .normal)
+        button.setTitle("button.", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.setBackgroundColor(.black, for: .normal)
         button.frame = CGRect(x: 0, y: 0, width: 32, height: 32)
-        let keyWindow = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
-        keyWindow?.addSubview(button)
+        
+        let allScenes = UIApplication.shared.connectedScenes
+        let scene = allScenes.first { $0.activationState == .foregroundActive }
+                                
+        if let windowScene = scene as? UIWindowScene {
+                 windowScene.keyWindow?.addSubview(button)
+        }
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+      
     }
 }
